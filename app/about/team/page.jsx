cf523111914b6be7bfe1b5194e68917f23dd5c6c
Annie Huang <annie.hn@gmail.com>
2023-04-06 21:04:59 +1000

    How to create nested route page -> keep creating folder and have a page.jsx inside.

---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci culpa
+        cumque enim eveniet ipsam non numquam. Accusantium aliquid asperiores
+        dolore doloribus maiores, nemo nobis odit, officia optio repudiandae
+        sequi tempore!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
