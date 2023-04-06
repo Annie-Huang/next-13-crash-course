24c1297d45765b003ab53905005aa679edd9375c
Annie Huang <annie.hn@gmail.com>
2023-04-06 21:20:08 +1000

    How to create MetaData for specific page. Do it in the page file or layout file. But keep in mind it will to trickle into the children component for Metadata.

--- app/about/layout.jsx
+++ app/about/layout.jsx
@@ -1,7 +1,15 @@
+// // A custom Metadata can put into here too
+// export const metadata = {
+//   title: 'Traversy Media',
+//   description: 'Web development tutorials and courses',
+//   keywords:
+//     'web development, web design, javascript, react, node, angular, vue, html, css',
+// };
+
 const AboutLayout = ({ children }) => {
   return (
     <div>
-      <h1>THIS IS THE ABOUT LAYOUT</h1>
+      {/*<h1>THIS IS THE ABOUT LAYOUT</h1>*/}
       {children}
     </div>
   );
