15061083f928cf2d3b31671efbd55e848150c8b3
Annie Huang <annie.hn@gmail.com>
2023-04-06 21:58:17 +1000

    Create a code/repos/ page. Add a fetchRepos async function.

---
+++ app/code/repos/page.jsx
@@ -0,0 +1,13 @@
+async function fetchRepos() {
+  const response = await fetch(
+    'https://api.github.com/users/bradtraversy/repos'
+  );
+  const repos = await response.json();
+  return repos;
+}
+
+const ReposPage = () => {
+  return <div>ReposPage</div>;
+};
+
+export default ReposPage;
