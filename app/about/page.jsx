b0c682a171f1e33ec8c008b08b7524c52d5e0131
Annie Huang <annie.hn@gmail.com>
2023-04-06 21:01:51 +1000

    How to create a page that under specific URL

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A assumenda
+        beatae blanditiis doloremque officiis praesentium repudiandae rerum
+        suscipit voluptatem? Aliquid at blanditiis cumque delectus error et
+        eveniet fugit illo, impedit ipsam minima molestias nam nesciunt possimus
+        quidem quis quod reiciendis similique sint sit tempora tempore tenetur
+        ut veritatis vero? Sit.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
