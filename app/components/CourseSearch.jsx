8158f6f93aae9c7d59e88a547cf71641a75ea504
Annie Huang <annie.hn@gmail.com>
2023-04-07 22:12:18 +1000

    Create a client component CourseSearch

---
+++ app/components/CourseSearch.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState } from 'react';
+
+const CourseSearch = () => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='search-form'>
+      <input
+        type='text'
+        className='search-input'
+        placeholder='Search Courses...'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type='submit' className='search-button'>
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default CourseSearch;
