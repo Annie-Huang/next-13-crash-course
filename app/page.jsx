373802ae7f83d432e70cc1bd1b57b05e20a8d0db
Annie Huang <annie.hn@gmail.com>
2023-04-07 21:56:12 +1000

    Call fetchCourses in useEfffect during page load at the root page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,29 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
+
+// This one need to import manually. the app/loading.jsx will only auto used by server component.
+import LoadingPage from './loading';
+
 import Courses from './components/Courses';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      console.log('data=', data);
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <div>
       <h1>Welcome To Traversy Media</h1>
